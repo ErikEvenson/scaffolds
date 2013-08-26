93b8256949ccfb8f82844671854697bc195d646c
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-26 01:11:46 -0500

    Closes #4.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,21 @@
+'use strict';
+
 module.exports = function(grunt){
     require('load-grunt-tasks')(grunt);
     
     grunt.initConfig({
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                '{,*/}*.js',
+            ]
+        },
         pkg: grunt.file.readJSON('package.json')
     });
     
     grunt.registerTask('default', function(){
-        console.log('Grunt default task enabled.')
+        console.log('Grunt default task enabled.');
     });
 };
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+'use strict';
 var http = require('http');
 
 http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
 }).listen(8124);
 
