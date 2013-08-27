e10cd24121c9367b08c054ff59f20b09c063c60e
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-26 19:13:44 -0500

    Closes #6.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,14 @@
 module.exports = function(grunt){
     require('load-grunt-tasks')(grunt);
     
+    var config = {
+        dist: 'dist',
+        herokuAppName: 'scaffolds'
+    };
+    
     grunt.initConfig({
+        config: config,
+        
         jshint: {
             options: {
                 jshintrc: '.jshintrc'
@@ -12,10 +19,39 @@ module.exports = function(grunt){
                 '{,*/}*.js',
             ]
         },
-        pkg: grunt.file.readJSON('package.json')
+        
+        pkg: grunt.file.readJSON('package.json'),
+        
+        shell: {
+            deploy: {
+                options: {
+                    stdout: true,
+                    stderr: true
+                },
+                command: [
+                    'echo == Removing old dist directory...',
+                    'rm -rf <%= config.dist %>',
+                    'echo == Creating new dist directory...',
+                    'mkdir <%= config.dist %>',
+                    'cd <%= config.dist %>',
+                    'echo == Cloning heroku repo...',
+                    'git clone git@heroku.com:scaffolds.git .',
+                    'rm -rf *',
+                    'echo == Deploying updates to heroku...',
+                    'cp ../server.js ../package.json ../Procfile .',
+                    'git add .',
+                    'git commit -m "mod"',
+                    'git push origin master'
+                ].join('&&')
+            }
+        }
     });
     
     grunt.registerTask('default', function(){
         console.log('Grunt default task enabled.');
     });
+    
+    grunt.registerTask('deploy', [
+        'shell:deploy'
+    ]);
 };
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
+    response.end('Scaffolds\n');
 }).listen(port);
 
 console.log('Server running at http://127.0.0.1:' + port);
