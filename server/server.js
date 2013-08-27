741381b29ba0d68afac3e5f3fd1e6e12e35ae1b1
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-27 00:51:31 -0500

    Refactor server.

--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ var express = require('express');
 var server = new express();
 var port = process.env.PORT || 5000;
 
+server.set('view engine', 'jade');
+server.set('views', './app/views');
+server.use(express.static('./app/public'));
+
+server.get('/', function(request, response){
+    response.render('index');
+});
+
 http.createServer(server).listen(port, function(){
     console.log('Server started.');
 });
 
-server.get('/', function(request, response){
-    response.send('Welcome!');
-});
