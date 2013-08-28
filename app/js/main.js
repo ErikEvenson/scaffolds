439a7e4cd83fa338dd685b19c4e2a5990e7f2fa6
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-27 21:57:58 -0500

    Checkpoint

--- app/js/main.js
+++ app/js/main.js
@@ -6,25 +6,34 @@ require.config({
         backbone: '../bower_components/backbone/backbone',
         bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
         jquery: '../bower_components/jquery/jquery',
+        json2: '../bower_components/json2/json2',
         marionette: '../bower_components/marionette/lib/backbone.marionette',
         underscore: '../bower_components/underscore/underscore'
     },
     shim: {
         backbone: {
-            deps: ['underscore', 'jquery'],
+            deps: [
+                'underscore',
+                'jquery',
+                'json2'
+            ],
             exports: 'Backbone'
         },
         marionette: {
-            deps: ['backbone'],
+            deps: [
+                'backbone'
+            ],
             exports: 'Marionette'
         },
         underscore: {
-            deps: ['jquery'],
+            deps: [
+                'jquery'
+            ],
             exports: '_'
         }
     }
 });
 
-require(['app'], function(App){
+require(['jquery', 'app'], function($, App){
     App.start();
 });
