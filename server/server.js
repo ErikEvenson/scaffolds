cdc875d21d2f2ebe3e4f049d4d272e33f568ba7c
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-29 10:45:55 -0500

    Checkpoint

--- server/server.js
+++ server/server.js
@@ -6,29 +6,15 @@ var server = new Percolator({
     'port': port
 });
 
-server.route('/', function(req, res){
-    res.object({hello: 'world!'}).send();
+server.route('/', {
+    GET: function(req, res){
+        res.object({
+            message: 'Hello World!'
+        }).send();
+    }
 });
 
 server.listen(function(err){
     if(err){throw err;}
     console.log('Percolator is listening on port ', server.port);
 });
-
-
-// var http = require('http');
-// var express = require('express');
-// var server = new express();
-
-// 
-// server.set('view engine', 'jade');
-// server.set('views', './app/views');
-// server.use(express.static('./app'));
-// 
-// server.get('/', function(request, response){
-//     response.render('index');
-// });
-// 
-// http.createServer(server).listen(port, function(){
-//     console.log('Server started.');
-// });
