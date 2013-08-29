4d6cf8fd7e3fda2ee66db98ba463da03dcd38173
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-29 11:09:50 -0500

    Adds staticDir and other percolator options

--- server/server.js
+++ server/server.js
@@ -1,12 +1,19 @@
 'use strict';
 
+var path = require('path');
 var Percolator = require('Percolator').Percolator;
 var port = process.env.PORT || 5000;
+var staticDir = path.join(__dirname, '../app');
+console.log(staticDir);
+
 var server = new Percolator({
-    'port': port
+    'port': port,
+    'protocol': 'http',
+    'resourcePath': '/api',
+    'staticDir': staticDir
 });
 
-server.route('/', {
+server.route('/api', {
     GET: function(req, res){
         res.object({
             message: 'Hello World!'
