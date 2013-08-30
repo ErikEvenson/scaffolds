5eb2fd5c72ec603ca166251b897fcb3682ea2205
Erik Evenson <erik.e.evenson@gmail.com>
2013-08-30 15:52:37 -0500

    Checkpoint

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,37 @@ define(['backbone', 'marionette'], function(Backbone, Marionette){
         mainRegion: '#main-region'
     });
     
+    App.navigate = function(route, options){
+        if (!options) {
+            options = {};
+        }
+
+        Backbone.history.navigate(route, options);
+    };
+    
+    App.getCurrentRoute = function(){
+        return Backbone.history.fragment;
+    };
+    
+    App.startModule = function(moduleName, args){
+        var currentModule = moduleName ? App.module(moduleName) : null;
+        
+        if (App.currentModule !== currentModule){
+            if (App.currentModule){
+                App.currentModule.stop();
+            }
+            
+            App.currentModule = currentModule;
+            
+            if (currentModule){
+                currentModule.start(args);
+            }
+        }
+    };
+    
     App.on('initialize:after', function(){
         if (Backbone.history) {
-            require(['apps/widgets/widgets'], function(){
+            require(['modules/widgets/module'], function(){
                 Backbone.history.start();
             });
         }
